bbcceae0b572b6a8d02e501a78938ab6fe4ce8c3
Bilal Al-Dulaimi <bilal.aldulaimi@yahoo.com>
2021-01-18 20:05:29 +0200

    Initial commit

---
+++ express_js/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Bilal Al-Dulaimi',
+        email: 'bilal.aldulaimi@yahoo.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Muhammad Al-Dulaimi',
+        email: 'muhammad.aldulaimi@yahoo.com',
+        status: 'inacive'
+    },
+    {
+        id: 3,
+        name: 'Ahmed Al-Dulain',
+        email: 'ahmed.aldulaimi@yahoo.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
